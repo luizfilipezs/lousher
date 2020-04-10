89575091410e58ec73a2eea252e0f155609be803
luizfilipezs <luizfilipe.tj@icloud.com>
2020-04-10 18:49:07 -0300

    Endereco interface

---
+++ lousher/src/app/endereco.ts
@@ -0,0 +1,11 @@
+export interface Endereco {
+  id?: number;
+  cep: string;
+  bairro: string;
+  rua: string;
+  numero: number;
+  complemento: string;
+  telefone: string;
+  email: string;
+  nomeDestinatario: string;
+}
--- lousher/src/app/home/home.component.ts
+++ lousher/src/app/home/home.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+//import { Endereco } from '../endereco';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjoxNTg2NzI3NzM2LCJlbWFpbCI6ImZpbGlwZWx1aXouYnNAZ21haWwuY29tIiwib3JpZ19pYXQiOjE1ODY1NTQ5MzZ9.qf5IG8nDSFRS_CCrerfaMSPLGHwq2yj9tkDbw8FPyoc'
+  })
+};
+
+const apiRoot = 'http://localhost:8000/api';
+const json = response => response.json();
 
 @Component({
   selector: 'app-home',
@@ -7,46 +19,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.postServer();
+    //this.getServer();
   }
-
-
+  
 	getServer(): void {
-    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjoxNTg2NjQ4OTk5LCJlbWFpbCI6ImZpbGlwZWx1aXouYnNAZ21haWwuY29tIiwib3JpZ19pYXQiOjE1ODY0NzYxOTl9.-ij7djmsKfK0fLit1JRN5Yt4tojF9upKSdYomq9oId0';
-    const apiRoot = 'http://localhost:8000/api';
-    const json = response => response.json();
-    const headers = new Headers({
-      "Authorization": `JWT ${token}`
-    });
-
-    fetch(`${apiRoot}/enderecos`, { headers: headers })
-      .then(json)
-			.then(console.log);
+    this.http.get(apiRoot.concat('/enderecos'), httpOptions)
+      .subscribe(
+        (success) => console.log('SUCCESS: ', success),
+        (error) => console.log('ERROR: ', error)
+      );
 	}
 
 	postServer(): void {
-    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjoxNTg2NjQ4OTk5LCJlbWFpbCI6ImZpbGlwZWx1aXouYnNAZ21haWwuY29tIiwib3JpZ19pYXQiOjE1ODY0NzYxOTl9.-ij7djmsKfK0fLit1JRN5Yt4tojF9upKSdYomq9oId0';
-    const apiRoot = 'http://localhost:8000/api';
-    const json = response => response.json();
-    const headers = new Headers({
-      "Authorization": `JWT ${token}`
-    });
-		
-		const body = new FormData();
-		body.append('cep', '00000-000');
-		body.append('bairro', 'centro');
-		body.append('rua', 'Lesgal');
-		body.append('numero', 70);
-		body.append('complemento', 'Ap. 101');
-		body.append('telefone', '(47) 9 9976-2919');
-		body.append('email', 'email@email.com');
-		body.append('nome_destinatario', 'Lima Barreto');
-
-    fetch(`${apiRoot}/enderecos`, { method: 'POST', headers: headers, body: body })
-      .then(json)
-			.then(console.log);
+    /*
+    const endereco: Endereco = {
+      cep: '00000-000',
+  		bairro: 'Centro',
+  		rua: 'Lesgal',
+  		numero: 70,
+  		complemento: 'Ap. 101',
+  		telefone: '(47) 9 9976-2919',
+  		email: 'email@email.com',
+  		nomeDestinatario: 'Lima Barreto'
+    }
+    
+    this.http.post<Endereco>(apiRoot.concat('/enderecos'), endereco, httpOptions)
+      .subscribe(
+        (success) => console.log('SUCCESS: ', success),
+        (error) => console.log('ERROR: ', error)
+      );
+    */
 	}
 }
